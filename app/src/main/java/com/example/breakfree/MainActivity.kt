d413232c29d8fc21e35aa045735857bc41954dde
chad-raven <chadraven.molina@gmail.com>
2024-03-12 22:38:57 +0800

    Modified login button.

--- app/src/main/java/com/example/breakfree/MainActivity.kt
+++ app/src/main/java/com/example/breakfree/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.breakfree
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding.bo
+
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+
+            btnLogin.setOnClickListener{
+
+                val intent = Intent(this, Dashboard::class.java)
+
+                startActivity(intent)
+            }
+
+
+
     }
 }
